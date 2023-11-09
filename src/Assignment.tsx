e1947a180d38cc70a0b44e20b6a0a5827aa75716
harshit bhorga <bhorgaji786@gmail.com>
2023-11-09 21:07:48 +0530

    enhanced code

--- src/Assignment.tsx
+++ src/Assignment.tsx
@@ -5,19 +5,50 @@ import DropdownHover from "./components/DropdownHover";
 const Assignment = (props: any) => {
 
     const [showDropdownOption, setShowDropdownOption] = useState<boolean>(false);
+    const rotateRef = useRef<any>(null);
+    const openAccountMenuRef = useRef<any>(null);
+    const accountMenuRef = useRef<any>(null);
 
     const openLocationDropdown = () => {
         setShowDropdownOption(true)
     }
 
 
-    const [sideNavMenu, setSideNavMenu] = useState([{ name: 'Buy a car', popular: ['Maruti', 'Hyundai', 'Honda', 'Renault', 'Tata', 'Toyota'], models:[] }])
-    const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
-    
+    const [sideNavMenu, setSideNavMenu] = useState([
+        { name: 'BUY A CAR', submenu: { popular: ['Maruti', 'Hyundai', 'Honda', 'Renault', 'Tata', 'Toyota'], models: [] } },
+        { name: 'Sell A CAR ' },
+        { name: 'CAR FINANCE' }
+    ])
+
+
+    useEffect(() => {
+        const handleMouseEnter = () => {
+            rotateRef.current.classList.add('rotate-180')
+            accountMenuRef.current.classList.remove('hidden');
+            accountMenuRef.current.classList.remove('opacity-0');
+            accountMenuRef.current.classList.add('opacity-100');
+        };
+
+        const handleMouseLeave = () => {
+            rotateRef.current.classList.remove('rotate-180');
+            accountMenuRef.current.classList.add('hidden');
+            accountMenuRef.current.classList.add('opacity-0');
+            accountMenuRef.current.classList.remove('opacity-100');
+        };
+
+        openAccountMenuRef.current.addEventListener('mouseenter', handleMouseEnter);
+        openAccountMenuRef.current.addEventListener('mouseleave', handleMouseLeave);
+
+        return () => {
+            openAccountMenuRef.current.removeEventListener('mouseenter', handleMouseEnter);
+            openAccountMenuRef.current.removeEventListener('mouseleave', handleMouseLeave);
+        };
+    }, []);
+
     return (<>
         <div className="h-screen overflow-auto bg-gray">
             <div className="bg-white px-2 relative border sticky top-0 z-10">
-                <div className="flex items-center grid grid-cols-12">
+                <div className="grid grid-cols-12">
                     <div className="col-span-3 pl-3 ml-3 py-4">
                         <div className="flex justify-center items-center">
                             <img src="https://fastly-production.24c.in/cars24/seo/static/1_20230830_1693395013.png" alt="" style={{ height: '39px' }} />
@@ -31,28 +62,72 @@ const Assignment = (props: any) => {
 
                     </div>
                     <div className="col-span-5 pl-10 items-center">
-                        <div className="flex ">
+                        <div className="grid grid-cols-4 pt-5">
                             {sideNavMenu.map((res: any) => {
                                 return <DropdownHover data={res} />
-                            })
-                            }
+                            })}
                         </div>
 
                     </div>
                     <div></div>
-                    <div className="col-span-3">
-                        how r u?
+                    <div className="col-span-3 py-6 mx-10">
+                        <div className="flex items-center justify-center mt-1">
+
+                            <div className="mr-5">
+                                <img src="https://assets.cars24.com/production/c2b-website/231107155239/js/a7b5a250e71331e76e52ae4c1ac245d4.svg" alt="Add to wishlist" width="24" height="24" />
+                            </div>
+                            <div className="mr-3">
+                                <img width="24" height="24" src="https://assets.cars24.com/production/c2b-website/231107155239/js/1dbd974090c35e48bdb8c3273b07fb4e.svg" alt="User" />
+                            </div>
+                            <div className="relative">
+                                <div className="absolute h-20 top-[-20px]" ref={openAccountMenuRef} >
+                                    <div className="font-bold ">
+                                        <div className="text-xs whitespace-nowrap text-[#465166]">
+                                            Hello, Sign in
+                                        </div>
+                                        <div className="text-lg flex">
+                                            <span className="mr-3">
+                                                Account
+                                            </span>
+                                            <img ref={rotateRef} className="transition-tansform duration-300" width="18" height="19" src="https://assets.cars24.com/production/c2b-website/231107155239/js/88757aed7f24280576e80d03627baf79.svg" alt="Account" />
+
+                                        </div>
+                                    </div>
+                                </div>
+
+
+                                <div id={'navDropdown'} ref={accountMenuRef} className="h-full absolute top-[50px] right-0 h-20 w-24 start-0 opacity-0">
+                                    <div className="bg-white p-4 rounded-lg">
+                                        <div className="pt-1 whitespace-nowrap">
+                                            <div className="col-span-3">
+                                                <div className="grid grid-cols-2">
+                                                    this is 
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+
+
+                            </div>
+
+
+                        </div>
+
+
+
                     </div>
                 </div>
 
 
             </div>
 
-{/****************************************************************************************************************** */}
+            {/****************************************************************************************************************** */}
 
             <div id={'navDropdown'} className="h-full opacity-50 z-10">
                 <div className="bg-slate-500 h-full">
-                    this is div under nav dropdown
+                    this is div under topbar
                 </div>
             </div>
 
--- src/components/DropdownHover.tsx
+++ src/components/DropdownHover.tsx
@@ -1,27 +1,35 @@
 import React, { useEffect, useRef } from "react";
 
-
-
 const DropdownHover = (props: any) => {
 
 
     const menuItemRef = useRef<any>(null);
+    const menuItemBorderRef = useRef<any>(null);
     const subMenuRef = useRef<any>(null);
+    const rotateRef = useRef<any>(null);
 
     useEffect(() => {
         const handleMouseEnter = () => {
-            subMenuRef.current.classList.remove('hidden');
-            subMenuRef.current.classList.remove('opacity-0');
-            //subMenuRef.current.classList.add('');
-            subMenuRef.current.classList.add('opacity-100');
-            console.log(subMenuRef.current.classList)
+            
+            menuItemBorderRef.current.classList.add('border-orange-500');
+            menuItemBorderRef.current.classList.remove('border-white');
+            if(subMenuRef.current){
+                rotateRef.current.classList.add('rotate-180')
+                subMenuRef.current.classList.remove('hidden');
+                subMenuRef.current.classList.remove('opacity-0');
+                subMenuRef.current.classList.add('opacity-100');
+            } 
         };
 
         const handleMouseLeave = () => {
-            //subMenuRef.current.classList.remove('');
-            subMenuRef.current.classList.remove('opacity-100');
-            subMenuRef.current.classList.add('hidden');
-            subMenuRef.current.classList.add('opacity-0');
+            menuItemBorderRef.current.classList.remove('border-orange-500');
+            menuItemBorderRef.current.classList.add('border-white');
+            if(subMenuRef.current){
+                rotateRef.current.classList.remove('rotate-180')
+                subMenuRef.current.classList.remove('opacity-100');
+                subMenuRef.current.classList.add('hidden');
+                subMenuRef.current.classList.add('opacity-0');
+            }  
         };
 
         menuItemRef.current.addEventListener('mouseenter', handleMouseEnter);
@@ -35,92 +43,110 @@ const DropdownHover = (props: any) => {
 
     return (
         <div className="lg:pt-1  whitespace-nowrap">
-            <div className="p-1 border-b-4 border-white hover:border-orange-500 rounded relative" ref={menuItemRef}>
-                {props.data.name}
-                <div id={'navDropdown'} ref={subMenuRef} className="h-full transition-opacity absolute top-[65px] -z-50 left-[-10px] opacity-100 duration-500 w-max">
-                    <div className="bg-white p-4 rounded-lg">
-                        <div className="grid grid-cols-8 pt-1">
-
-                            <div className="col-span-3">
-                                <div className="grid grid-cols-2">
-                                    <div>
-                                        <div className="font-semibold">
-                                            Popular Makes
-                                        </div>
-                                        {props.data.popular?.map((el: any) => {
-                                            return <div className="my-1">
-                                                {el}
-                                            </div>
-                                        })}
-
-                                    </div>
-                                    <div>
-                                        <div className="font-semibold">
-                                            Popular Models
-                                        </div>
-                                        {props.data.models?.map((el: any) => {
-                                            return <div className="my-1">
-                                                {el}
-                                            </div>
-                                        })}
-                                    </div>
-
-
-                                </div>
-
-                            </div>
-                            <div className="col-span-3">
-
-                                <div className="grid grid-cols-2">
-                                    <div>
-                                        <div className="font-semibold">
-                                            Popular Makes
-                                        </div>
-                                        {props.data.popular?.map((el: any) => {
-                                            return <div className="my-1">
-                                                {el}
-                                            </div>
-                                        })}
-
-                                    </div>
-                                    <div>
-                                        <div className="font-semibold">
-                                            Popular Models
-                                        </div>
-                                        {props.data.models?.map((el: any) => {
-                                            return <div className="my-1">
-                                                {el}
-                                            </div>
-                                        })}
-                                    </div>
-
-
-                                </div>
-
-                            </div>
-                            <div className="col-span-2">
-                                <div className="grid grid-cols-2 gap-4">
-                                    <div>
-                                        <div className="font-semibold">
-                                            Popular Makes
-                                        </div>
-                                        {props.data.popular?.map((el: any) => {
-                                            return <div className="my-1">
-                                                {el}
-                                            </div>
-                                        })}
-
-                                    </div>
-
-
-
-                                </div>
-
-                            </div>
-
+            <div className="relative" >
+                
+                <div className="absolute h-20" ref={menuItemRef}>
+                    <div className="p-1 border-b-4 font-semibold rounded border-white text-sm flex" ref={menuItemBorderRef}>
+                        {props.data.name}
+                        {props.data.submenu && <div className="transition-tansform duration-300 ml-1" ref={rotateRef}>
+                            <img width="24" height="24"  src="https://assets.cars24.com/production/c2b-website/231107155239/js/bf6340572e7389b8b545ad752218ee5c.svg" alt="arrow"/>
                         </div>
+                        }
+
                     </div>
+
+                    {props.data.submenu && <div id={'navDropdown'} ref={subMenuRef} className="h-full  absolute top-[65px] left-[-10px] opacity-0  w-max">
+                        <div className="bg-white p-4 rounded-lg">
+                            <div className="grid grid-cols-8 pt-1">
+
+                                <div className="col-span-3">
+                                    <div className="grid grid-cols-2">
+                                        <div>
+                                            <div className="font-semibold">
+                                                Popular Makes
+                                            </div>
+                                            {props.data.submenu?.popular?.map((el: any) => {
+                                                return <div className="my-1">
+                                                    {el}
+                                                </div>
+                                            })}
+
+                                        </div>
+                                        <div>
+                                            <div className="font-semibold">
+                                                Popular Models
+                                            </div>
+                                            {props.data.submenu?.models?.map((el: any) => {
+                                                return <div className="my-1">
+                                                    {el}
+                                                </div>
+                                            })}
+                                        </div>
+
+
+                                    </div>
+
+                                </div>
+                                <div className="col-span-3">
+
+                                    <div className="grid grid-cols-2">
+                                        <div>
+                                            <div className="font-semibold">
+                                                Popular Makes
+                                            </div>
+                                            {props.data.submenu?.popular?.map((el: any) => {
+                                                return <div className="my-1">
+                                                    {el}
+                                                </div>
+                                            })}
+
+                                        </div>
+                                        <div>
+                                            <div className="font-semibold">
+                                                Popular Models
+                                            </div>
+                                            {props.data.submenu.models?.map((el: any) => {
+                                                return <div className="my-1">
+                                                    {el}
+                                                </div>
+                                            })}
+                                        </div>
+
+
+                                    </div>
+
+                                </div>
+                                <div className="col-span-2">
+                                    <div className="grid grid-cols-2 gap-4">
+                                        <div>
+                                            <div className="font-semibold">
+                                                Popular Makes
+                                            </div>
+                                            {props.data.popular?.map((el: any) => {
+                                                return <div className="my-1">
+                                                    {el}
+                                                </div>
+                                            })}
+
+                                        </div>
+
+
+
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+                    </div>}
+                
                 </div>
+                
+                
+                
+                
+                
+                
 
             </div>
         </div>
